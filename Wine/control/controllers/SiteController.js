f367a28acd156f957cb99e7a2bf3a55f48eff731
hyung <viethung4869@gmail.com>
2022-08-04 00:03:21 +0700

    first commit

--- Wine/control/controllers/SiteController.js
+++ Wine/control/controllers/SiteController.js
@@ -2,16 +2,49 @@
 
 class SiteController{
 
-  //get news
+  //get /home/
   index(req, res, next){
-
-      return res.render('home')
-
+    res.render('home');
   }
 
-  //get /search/
+  //get /about/
   about(req, res, next){
     res.render('about');
+  }
+
+  //get /product/
+  product(req, res, next){
+    res.render('products');
+  }
+
+  //get /detail-product/
+  productDetail(req, res, next){
+    res.render('product-detail');
+  }
+
+  //get /blog/
+  blog(req, res, next){
+    res.render('blog');
+  }
+
+  //get /blog-detail/
+  blogDetail(req, res, next){
+    res.render('blog-detail');
+  }
+
+  //get /cart/
+  cart(req, res, next){
+    res.render('cart');
+  }
+
+  //get /checkout/
+  checkout(req, res, next){
+    res.render('checkout');
+  }
+
+  //get /contact/
+  contact(req, res, next){
+    res.render('contact');
   }
 }
 
--- Wine/routes/site.js
+++ Wine/routes/site.js
@@ -3,6 +3,13 @@ const router = express.Router();
 
 const siteController = require('../control/controllers/SiteController');
 
+router.get('/blog',siteController.blog);
+router.get('/blog-detail',siteController.blogDetail);
+router.get('/cart',siteController.cart);
+router.get('/checkout',siteController.checkout);
+router.get('/contact',siteController.contact);
+router.get('/product-detail',siteController.productDetail);
+router.get('/product',siteController.product);
 router.get('/about',siteController.about);
 router.get('/',siteController.index);
 
